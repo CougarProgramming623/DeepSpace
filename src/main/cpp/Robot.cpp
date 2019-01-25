335be6437a25fa57c98ac0afde32a3c15e0d20c2
Troy Neubauer <troyneubauer@gmail.com>
2019-01-24 20:54:42 -0500

    Updated files to be compliant with coding standards

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,13 +1,9 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 
 #include "Robot.h"
 #include <frc/DriverStation.h>
 #include <WPILib.h>
+
+namespace frc2019 {
 
 std::shared_ptr<DriveTrain> Robot::driveTrain;
 std::shared_ptr<AHRS> Robot::navx;
@@ -24,7 +20,7 @@ void Robot::RobotInit() {
 
 	CameraServer::GetInstance()->StartAutomaticCapture();
 }
-
+    
 void Robot::AutonomousInit() {
 
 }
@@ -55,3 +51,6 @@ int main() {
 	return frc::StartRobot<Robot>();
 }
 #endif
+
+
+}//frc2019
