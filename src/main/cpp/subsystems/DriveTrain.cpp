b3981749c907afc25476cb994834a21156132bf2
Troy Neubauer <troyneubauer@gmail.com>
2019-01-29 15:19:13 -0500

    Added Operator Interface template

--- src/main/cpp/subsystems/DriveTrain.cpp
+++ src/main/cpp/subsystems/DriveTrain.cpp
@@ -12,18 +12,17 @@ DriveTrain::DriveTrain() : Subsystem("DriveTrain") {
 	mRightFrontMC.reset(new WPI_TalonSRX(RIGHT_FRONT_ID));
 	mLeftRearMC.reset(new WPI_TalonSRX(LEFT_BACK_ID));
 	mRightRearMC.reset(new WPI_TalonSRX(RIGHT_BACK_ID));
-	mMecanumDrive.reset(new frc::MecanumDrive(*mLeftFrontMC, *mRightFrontMC,*mLeftRearMC, *mRightRearMC));
+	mMecanumDrive.reset(new frc::MecanumDrive(*mLeftFrontMC, *mRightFrontMC,*mLeftRearMC, *mRightRearMC)); 
 	mMecanumDrive->SetExpiration(0.1);
 	mMecanumDrive->SetSafetyEnabled(false);
-	mMecanumDrive->SetMaxOutput(1.0);
-	
+	mMecanumDrive->SetMaxOutput(1.0);	
 }
 
 void DriveTrain::InitDefaultCommand() {
 	// Set the default command for a subsystem here.
 	SetDefaultCommand(new Drive());
 }
-
+ 
 void DriveTrain::fodDrive(double y, double x, double rotation, double gyroAngle) {
 	mMecanumDrive->DriveCartesian(y, x, rotation, gyroAngle);
 }
