6d6c045ae3acbf53c9c6d4b8e7d1930a87c43bea
Ronnie <ronniecm@gmail.com>
2019-02-17 17:27:29 -0500

    Divided rotation power by 2

--- src/main/cpp/commands/Drive.cpp
+++ src/main/cpp/commands/Drive.cpp
@@ -43,13 +43,13 @@ void Drive::Execute() {
 
 	if (Robot::oi->IsFOD()) {
 		//Robot::driveTrain->FODDrive(y, x, rot, gyro);
-		Robot::driveTrain->CartesianDrive(y, x, rot, gyro);
+		Robot::driveTrain->CartesianDrive(y, x, rot/2, gyro);
 	} else {// Is alignment
 		// Disabled for now - dims down the effect of x and rot
 		//x = ReduceValue(x, 3.0);
 		//rot = ReduceValue(rot, 2.0);
 		//Robot::driveTrain->FODDrive(y, x, rot, 0.0);
-		Robot::driveTrain->CartesianDrive(y, x, rot, 0.0);
+		Robot::driveTrain->CartesianDrive(y, x, rot/2, 0.0);
 	}
 }
 
