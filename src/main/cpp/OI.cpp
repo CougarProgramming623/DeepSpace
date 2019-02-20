ba8e1808f3ab01894a20bda02d8ba151466d6df0
Carson Graham <carson42g@gmail.com>
2019-02-19 19:19:43 -0500

    added acedentally removed lines of code from master

--- src/main/cpp/OI.cpp
+++ src/main/cpp/OI.cpp
@@ -26,8 +26,12 @@ OI::OI() :
 			// frc::DriverStation::ReportError(std::string("LAMBDA TEST FOD: ") + (newValue ? "true" : "false"));
 			Cob::PushValue(COB_FIELD_ORIENTED,newValue);
 		}));
-	}
 	
+		low.WhenPressed(new SetForkPosition(300));
+		medium.WhenPressed(new SetForkPosition(0));
+		high.WhenPressed(new SetForkPosition(150));
+	}
+
 
 
 void OI::Update() {
